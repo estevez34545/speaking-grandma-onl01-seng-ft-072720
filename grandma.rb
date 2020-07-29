e002a5499a7cd948a9800f1e532a3d3ece8b0fb4
lisbeth estevez <estevez34545@gmail.com>
2020-07-29 22:24:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,6 +6,7 @@ def speak_to_grandma(phrase)
 
 end
 
+speak_to_grandma("Hello world")
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
