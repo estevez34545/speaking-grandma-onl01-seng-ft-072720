1f094fec27108ac0318746daea3d7b7291d72881
lisbeth estevez <estevez34545@gmail.com>
2020-07-29 23:00:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,8 @@ def speak_to_grandma(phrase)
    "I LOVE YOU TOO PUMPKIN!"
    elsif phrase == phrase.upcase
    "NO, NOT SINCE 1938!"
- else HUH?! SPEAK UP, SONNY!
+ else 
+   "HUH?! SPEAK UP, SONNY!"
  end
   
 
